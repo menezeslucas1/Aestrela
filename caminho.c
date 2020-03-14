3e1d2f7983084fc24a1fc173a66687e15cf3b91b
menezeslucas1 <menezeslucas47@gmail.com>
2020-03-14 19:47:29 -0300

    falha de segmentação

--- caminho.c
+++ caminho.c
@@ -5,20 +5,39 @@
 
 #define qtdCidade 20
 
-struct vizinha
-{
-	struct cidade *cidade;
-	int distancia;
-};
-
 struct cidade
 {
 	char nome[20];
-	int distEstimada;
-	struct vizinha vizinha[20];
+	int distanciaDestino;
+	struct cidade *vizinha[20];
+	int distancia[20];
 };
-
 typedef struct cidade Cidade;
+
+struct etapa
+{
+	Cidade *cidade;
+	int g;
+	int h;
+	struct etapa *anterior;
+};
+typedef struct etapa Etapa;
+
+struct lista
+{
+	Etapa *etapa;
+	struct lista *proximo;
+};
+typedef struct lista Lista;
+
+void leDistancia(FILE *arquivo, Cidade *cidade, int qtd);
+Cidade * procura(char *nome, Cidade *cidade, int qtd);
+void leNome(FILE *arquivo, char *nome);
+void leMapa(FILE *arquivo, Cidade *mapa, int qtd);
+void remover1(Lista **borda);
+void expandir(Lista **borda, Cidade **cidade, int *distancia);
+void adicionar(Lista **inicio, Etapa *etapa);
+void menorCaminho(char *nomeOrigem, Cidade *mapa, int qtd);
 
 void leDistancia(FILE *arquivo, Cidade *cidade, int qtd)
 {
@@ -34,7 +53,7 @@ void leDistancia(FILE *arquivo, Cidade *cidade, int qtd)
 			if(isdigit(aux[0]))
 			{
 				//Lê o valor da distância
-				cidade[i].distEstimada = atoi(aux);
+				cidade[i].distanciaDestino = atoi(aux);
 				break;
 			}
 			else{
@@ -43,7 +62,6 @@ void leDistancia(FILE *arquivo, Cidade *cidade, int qtd)
 				strcat(cidade[i].nome, aux);
 			}
 		}while  (s==1);
-//		printf("%20s\t%d\n", cidade[i].nome, cidade[i].distancia);
 	}
 }
 
@@ -54,6 +72,7 @@ Cidade * procura(char *nome, Cidade *cidade, int qtd)
 		if (strcmp(nome, cidade[i].nome)==0)
 			return &cidade[i];
 	}
+	printf("cidade não encontrada.\n");
 	return NULL;
 }
 
@@ -84,13 +103,127 @@ void leNome(FILE *arquivo, char *nome)
 	ungetc(c, arquivo);
 }
 
-void leMapa(FILE *arquivo, Cidade *cidade, int qtd)
+void leMapa(FILE *arquivo, Cidade *mapa, int qtd)
 {
+	char nome[20];
+	char vizinha[20];
+	int distancia;
+	Cidade *cidade;
+	Cidade *v;
+	int c;
+	int i=0;
+	for (int j=0; j<qtd; j++)
+	{
+		//Seleciona uma cidade do mapa
+		leNome(arquivo, nome);
+		cidade = procura(nome, mapa, qtd);
+		if (cidade == NULL)
+		{
+			printf("Impossível encontrar a cidade: %s\n", nome);
+			exit(1);
+		}
+		i=0;
+		//decobre as cidades vizinhas
+		do
+		{
+			//Descobre a distância até a cidade vizinha
+			fscanf(arquivo, "%d", &distancia);
+			//Descobre a cidade vizinha
+			leNome(arquivo, vizinha);
+			v = procura(vizinha, mapa, qtd);
+			if (v == NULL)
+			{
+				printf("Impossível encontrar a cidade: %s\n", vizinha);
+				exit(1);
+			}
+			cidade->vizinha[i] = v;
+			cidade->distancia[i] = distancia;
+			i++;
+			c = getc(arquivo);
+			if(c!='\n')
+				ungetc(c,arquivo);
+		}while(c!='\n');
+		cidade->vizinha[i]=NULL;
+	}
+}
 
+void remover1(Lista **borda)
+{
+	*borda = (*borda)->proximo;
+}
 
-//		printf("aff2\n");
-//	}
-//	printf("%s\t%d\n", c->vizinha[0].cidade->nome, c->vizinha[0].cidade->distEstimada);
+void expandir(Lista **borda, Cidade **cidade, int *distancia)
+{
+	int i=0;
+	int g = (**borda).etapa->g;
+//	int h = borda->etapa->h;
+	Etapa *etapa;
+	Etapa *anterior = (**borda).etapa;
+	remover1(borda);
+	while (cidade[i]!=NULL)
+	{
+		etapa = (Etapa *)malloc(sizeof(etapa));
+		etapa->cidade = cidade[i];
+		etapa->anterior = anterior;
+		etapa->g = g+distancia[i];
+		etapa->h = cidade[i]->distanciaDestino;
+		adicionar(borda, etapa);
+	}
+}
+
+void adicionar(Lista **inicio, Etapa *etapa)
+{
+	if (*inicio==NULL)
+	{
+		printf("nulo\n");
+		*inicio = (Lista *)malloc(sizeof(Lista));
+		(**inicio).etapa = etapa;
+		(**inicio).proximo = NULL;
+		return;
+	}
+	int fnovo = etapa->g + etapa->h;
+	int fantigo = (**inicio).etapa->g + (**inicio).etapa->h;
+	if (fnovo <= fantigo)
+	{
+		Lista *novo = (Lista *)malloc(sizeof(Lista));
+		novo->etapa = etapa;
+		novo->proximo = *inicio;
+		*inicio = novo;
+	}
+	else
+		adicionar(&(**inicio).proximo,etapa);
+}
+
+void menorCaminho(char *nomeOrigem, Cidade *mapa, int qtd)
+{
+	Cidade *origem = procura(nomeOrigem, mapa, qtd);
+	Etapa inicial = {origem,0,origem->distanciaDestino};
+	Lista *borda;
+	borda=NULL;
+
+//	if(borda==NULL)
+//		printf("nulo1\n");
+
+	adicionar(&borda, &inicial);
+//	printf("asdf\n");
+
+//	if(borda==NULL)
+//		printf("nulo2\n");
+
+	int i=0;
+	while (borda->etapa->cidade->distanciaDestino != 0)
+	{
+		expandir(&borda, origem->vizinha, origem->distancia);
+		if(++i >= 100)
+			break;
+	}
+	Etapa *e;
+	e = borda->etapa;
+	do
+	{
+		printf("%s\n",e->cidade->nome);
+		e = e->anterior;
+	}while(e!=NULL);
 }
 
 int main()
@@ -110,8 +243,8 @@ int main()
 		printf("erro ao abrir o arquivo de mapa.\n");
 		return 1;
 	}
-	
 	leMapa(mapa, cidade, qtdCidade);
+	menorCaminho("Lugoj", cidade, qtdCidade);
 	printf("Sucesso.\n");
 	return 0;
 }
